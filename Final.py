0501bc86f6b4e08e2ad50dbf51c33e9774bed702
Abdelrhman-Ghowil <104584928+Abdelrhman-Ghowil@users.noreply.github.com>
2024-06-21 22:29:46 +0300

    Final.py

--- Final.py
+++ Final.py
@@ -7,10 +7,6 @@ from PIL import Image, UnidentifiedImageError
 import re
 from transformers import pipeline
 
-
-
-
-
 # Function to convert Google Drive link to direct download link
 def convert_drive_link(link):
     match = re.search(r'/d/([^/]+)', link)
@@ -52,33 +48,47 @@ def remove_background(image_content):
         return None
 
 # Function to combine the foreground image with a background image
-def combine_with_background(foreground_content, background_content, resize_foreground=False):
+def combine_with_background(foreground_content, background_content, resize_foreground=False, scaling_factor=1.0):
     try:
         foreground = Image.open(BytesIO(foreground_content)).convert("RGBA")
         background = Image.open(BytesIO(background_content)).convert("RGBA")
         background = background.resize((1024, 1024))
-        
+
         if resize_foreground:
-            # Calculate the scaling factor
-            k = (400000 / int((foreground.width * foreground.height))) ** 0.5
-            foreground_size = (int(foreground.width * k), int(foreground.height * k))
-            foreground = foreground.resize(foreground_size)
+            # Calculate the scaling factor to cover a percentage of the background
+            fg_area = foreground.width * foreground.height
+            bg_area = background.width * background.height
+            scale_factor = (.8 * bg_area / fg_area) ** 0.5
+
+            new_width = int(foreground.width * scale_factor)
+            new_height = int(foreground.height * scale_factor)
+
+            if new_height > 1024 or new_width > 1024:
+                new_width = int(new_width * scaling_factor/100)
+                new_height = int(new_height * scaling_factor/100)
+
+            foreground = foreground.resize((new_width, new_height))
+
+            # Save the dimensions of the object
+            dimensions = (new_width, new_height)
+        else:
+            dimensions = (foreground.width, foreground.height)
 
         # Center the foreground on the background
         fg_width, fg_height = foreground.size
         bg_width, bg_height = background.size
         position = ((bg_width - fg_width) // 2, (bg_height - fg_height) // 2)
-        
+
         combined = background.copy()
         combined.paste(foreground, position, foreground)
         img_byte_arr = BytesIO()
         combined.save(img_byte_arr, format='PNG')
-        return img_byte_arr.getvalue()
+        return img_byte_arr.getvalue(), dimensions
     except UnidentifiedImageError:
-        return None
+        return None, None
 
 # Function to download all images as a ZIP file
-def download_all_images_as_zip(images_info, remove_bg=False, add_bg=False, bg_image=None, resize_foreground=False):
+def download_all_images_as_zip(images_info, remove_bg=False, add_bg=False, bg_image=None, resize_foreground=False, scaling_factor=1.0):
     zip_buffer = BytesIO()
     with ZipFile(zip_buffer, 'w') as zf:
         for name, url_or_file in images_info:
@@ -87,7 +97,7 @@ def download_all_images_as_zip(images_info, remove_bg=False, add_bg=False, bg_im
                 image_content = download_image(url)
             else:
                 image_content = url_or_file.read()
-            
+
             if image_content:
                 if remove_bg:
                     processed_image = remove_background(image_content)
@@ -95,75 +105,47 @@ def download_all_images_as_zip(images_info, remove_bg=False, add_bg=False, bg_im
                 else:
                     processed_image = resize_image(image_content)
                     ext = 'jpeg'
-                
+
                 if add_bg and bg_image:
-                    processed_image = combine_with_background(processed_image, bg_image, resize_foreground=resize_foreground)
+                    processed_image, dimensions = combine_with_background(processed_image, bg_image, resize_foreground=resize_foreground, scaling_factor=scaling_factor)
                     ext = 'png'
-                
+
                 if processed_image:
                     zf.writestr(f"{name}", processed_image)
     zip_buffer.seek(0)
     return zip_buffer
 
 # Streamlit UI
-#css upload file
 st.markdown("""
     <style>
-    .st-emotion-cache-1erivf3,.st-emotion-cache-1gulkj5 {
+    .st-emotion-cache-1erivf3, .st-emotion-cache-1gulkj5 {
        display: flex;
        -webkit-box-align: center;
        align-items: center;
        flex-direction: column;
        justify-content: space-around;
        height: 175px;
-       
-       }
-       
+    }
     </style>
-    """, unsafe_allow_html=True)
+""", unsafe_allow_html=True)
 
-# st.set_page_config(
-#     page_title="PhotoMaster",
-#     page_icon="🖼️"
-# )
-
-
-# ---------- HEADER ----------
 st.title("🖼️ PhotoMaster")
 
 # Page layout
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    # st.markdown("<div  width='200' height='200'>upload</div>")
-    # st.markdown("")
-    # Set page title and layout
-#st.set_page_config(page_title="Shobbak Tool", layout="wide")
+    uploaded_files = st.file_uploader("", type=["xlsx", "csv", "jpg", "jpeg", "png"], accept_multiple_files=True)
 
-# Custom CSS to style the buttons and other elements
-# st.markdown("""
-#     <style>
-#     .st-emotion-cache-1erivf3 {
-#        display: flex;
-#        -webkit-box-align: center;
-#        align-items: center;
-#        flex-direction: column;
-#        justify-content: space-around;
-#        height: 150px;
-       
-#        }
-       
-#     </style>
-#     """, unsafe_allow_html=True)
-
-    #uploaded_files = st.file_uploader("Upload an Excel file (xlsx/csv) or images (jpg/jpeg/png)", type=["xlsx", "csv", "jpg", "jpeg", "png"], accept_multiple_files=True)
-    uploaded_files = st.file_uploader("",type=["xlsx", "csv", "jpg", "jpeg", "png"], accept_multiple_files=True)
 with col2:
     st.markdown("")
     remove_bg = st.checkbox("Remove background")
     add_bg = st.checkbox("Add background")
     resize_fg = st.checkbox("Resize")
+    scaling_factor = st.slider("Scaling Factor for Foreground", 50, 100, 10)
     st.checkbox("Compress and Convert Format")
+    
+
 images_info = []
 if uploaded_files:
     if len(uploaded_files) == 1 and uploaded_files[0].name.endswith(('.xlsx', '.csv')):
@@ -182,13 +164,13 @@ if uploaded_files:
                 df = pd.read_excel(uploaded_file)
             else:
                 df = pd.read_csv(uploaded_file)
-            
+
             if 'links' in df.columns and ('name' in df.columns or 'names' in df.columns):
                 df.dropna(subset=['links'], inplace=True)
                 images_info = list(zip(df['name'], df['links']))
             else:
                 st.error("The uploaded file must contain 'links' and 'name' columns.")
-        
+
         elif file_type == 'images':
             images_info = [(file.name, file) for file in uploaded_files]
 
@@ -201,7 +183,7 @@ if images_info:
 
     st.markdown("## Preview")
     if st.button("Download All Images", key="download_all"):
-        zip_buffer = download_all_images_as_zip(images_info, remove_bg=remove_bg, add_bg=add_bg, bg_image=bg_image, resize_foreground=resize_fg)
+        zip_buffer = download_all_images_as_zip(images_info, remove_bg=remove_bg, add_bg=add_bg, bg_image=bg_image, resize_foreground=resize_fg, scaling_factor=scaling_factor)
         st.download_button(
             label="Download All Images as ZIP",
             data=zip_buffer,
@@ -218,7 +200,7 @@ if images_info:
                 image_content = download_image(url)
             else:
                 image_content = url_or_file.read()
-            
+
             if image_content:
                 if remove_bg:
                     processed_image = remove_background(image_content)
@@ -226,11 +208,11 @@ if images_info:
                 else:
                     processed_image = resize_image(image_content)
                     ext = 'jpeg'
-                
+
                 if add_bg and bg_image:
-                    processed_image = combine_with_background(processed_image, bg_image, resize_foreground=resize_fg)
+                    processed_image, dimensions = combine_with_background(processed_image, bg_image, resize_foreground=resize_fg, scaling_factor=scaling_factor)
                     ext = 'png'
-                
+
                 if processed_image:
                     st.image(processed_image, caption=name)
                     st.download_button(
@@ -239,4 +221,3 @@ if images_info:
                         file_name=f"{name}",
                         mime=f"image/{ext}"
                     )
-
