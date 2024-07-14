faeaa582f72ea5cf41ef5ed561a80d1601bf5e5a
Abdelrhman-Ghowil <104584928+Abdelrhman-Ghowil@users.noreply.github.com>
2024-07-15 00:24:40 +0300

    Final.py

--- Final.py
+++ Final.py
@@ -207,7 +207,7 @@ if images_info:
                 if remove_bg:
                     processed_image = remove_background(image_content)
                     ext = 'png'
-                if resize_fg:
+                else:
                     processed_image = resize_image(image_content)
                     ext = 'png'
 
